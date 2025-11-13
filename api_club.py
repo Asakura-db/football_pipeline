60e5cfc52de64357541d596c8b4f9e62bccd52fd
Yann B <57952703+YannBol@users.noreply.github.com>
2025-11-13 13:25:20 +0100

    Update api_club.py

--- api_club.py
+++ api_club.py
@@ -3,9 +3,10 @@ import requests
 API_KEY = "79df369c68fa417296dfdb3a5986ef6d"
 headers = {"X-Auth-Token": API_KEY}
 
-url = "https://api.football-data.org/v4/competitions/PL/teams"  # Exemple Premier League
+url = "https://api.football-data.org/v4/competitions/PL/teams" 
 response = requests.get(url, headers=headers)
 
 data = response.json()
 for team in data["teams"]:
     print(team["name"], "-", team["id"])
+
