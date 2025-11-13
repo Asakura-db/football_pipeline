1dfc14711ad941566e19af120839bd9c6ef99521
Yann B <57952703+YannBol@users.noreply.github.com>
2025-11-13 13:20:06 +0100

    Update api_joueur.py

--- api_joueur.py
+++ api_joueur.py
@@ -3,7 +3,7 @@ import requests
 API_KEY = "79df369c68fa417296dfdb3a5986ef6d"
 headers = {"X-Auth-Token": API_KEY}
 
-team_id = 66  # exemple : Manchester United
+team_id = 66  # Manchester United
 url = f"https://api.football-data.org/v4/teams/{team_id}"
 response = requests.get(url, headers=headers)
 
@@ -13,3 +13,4 @@ print("Joueurs :")
 
 for player in team_data["squad"]:
     print(player["name"], "-", player["position"], "-", player["nationality"])
+
